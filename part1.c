c5931b287527e8fc87ec08260e63bc8e9ea14bf8
Dan Garcia <dgarcia1@umassd.edu>
2015-12-02 13:22:57 -0500

    Everything but analysis loop done

--- part1.c
+++ part1.c
@@ -1,7 +1,13 @@
+#include <time.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <strings.h>
+#include <errno.h>
 
 typedef struct
 {
@@ -9,9 +15,17 @@ int value;
 }	shared_mem; shared_mem	*counter;
 /* This fcn should increase the value of the shared variable "counter" all the way
 up to 50000 */
-
+int *status;
 process1()
 {
+	printf("Child 1 started. The variable is %d\n",counter->value);
+	int i;
+	for(i = 0; i<100000;i++){
+		counter->value= counter->value +1;
+//		printf("%d ",counter->value);
+	}
+	printf("Child 1 done execution. The variable is %d\n",counter->value);
+//	exit(0);
 
 }
 
@@ -20,6 +34,16 @@ up to 50000 */
 
 process2()
 {
+//	sleep(1);
+	printf("Child 2 started. The variable is %d\n",counter->value);
+	int j;
+	usleep(150);
+	 for(j = 0; j<100000;j++){
+		counter->value = counter->value +1;
+//		printf("%d ",counter->value);
+	}
+	printf("Child 2 done execution. The variable is %d\n",counter->value);
+//	exit(0);
 
 }
 
@@ -30,13 +54,15 @@ main()
 
 
 
-memory */
+//memory //*/
 
 
-key_t	key = IPC_PRIVATE;	/* shared memory key */ int	shmid;	/* shared memory ID */ shared_mem	*shmat1;	/* function to allocate shared
+key_t key = IPC_PRIVATE;	/* shared memory key */ 
+int shmid;	/* shared memory ID */ 
+shared_mem *shmat1;	/* function to allocate shared*/
 
-int	pid1;	/* process id for child1 */
-int	pid2;	/* process id for child2 */
+int pid1;	/* process id for child1 */
+int pid2;	/* process id for child2 */
 
 
 /* attempts to attach to an existing memory segment	*/
@@ -48,8 +74,7 @@ perror("shmget"); exit(1);
 
 /*attempts the shared memory segment	*/
 
-if((counter = (shared_mem *)shmat(shmid, NULL, 0)) == (shared_mem *) -1)
-{
+if((counter = (shared_mem *)shmat(shmid, NULL, 0)) == (shared_mem *) -1){
 perror("shmat"); exit(1);
 }
 
@@ -57,20 +82,33 @@ perror("shmat"); exit(1);
 
 
 /* fork process one here */
-
+pid1 = fork();
+if(pid1 == 0){
+	//Im the baby
+	process1();
+}else{
 /* fork process two here */
+	 pid2 = fork();
+	if(pid2 == 0){
+		//I am the second child
+//		usleep(100);	
+		process2();
+	}else{
 
-/* parent waits (use wait();) for child processes to finish.
-*/
-
-/* parent process reports value of counter */
-
-/* parent process exits safely */
-
-/*deallocate shared memory */
+		/* parent waits (use wait();) for child processes to finish.*/
+		wait(&status);
+		wait(&status);
+		/* parent process reports value of counter */
+		printf("The counter variable is: %d\n",counter->value);
+		/* parent process exits safely */
+		exit(0);
+		/*deallocate shared memory */
+	}
+}
 if(shmctl(shmid, IPC_RMID, (struct shmid_ds *)0)== -1){ perror("shmctl");
-exit(-1);
+	exit(-1);
 }
 exit(0);
-
+		
+	
 }
